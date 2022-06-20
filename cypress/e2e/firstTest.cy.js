7711f8be4033211614a2fdbb176ce63d36293972
Alexandra Popova <alexandra.popova@quantori.com>
2022-06-20 13:35:58 +0300

    created a tsconfig file, added new script in package json file to open cypress

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,6 +1,7 @@
-describe('first test', () => {
-    it('sign in', () => {
-      cy.visit('http://automationpractice.com/index.php')
-      cy.title().should('contain', 'My Store');
-    })
-  })
+describe('first test', function () {
+    it('sign in', function () {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.title().should('contain', 'My Store');
+    });
+});
+//# sourceMappingURL=firstTest.cy.js.map
