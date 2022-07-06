6aa6cc57dc76fdc13457299d9419a3027a4a8899
Alexandra Popova <alexandra.popova@quantori.com>
2022-07-06 15:21:59 +0300

    removed plugins for prettier, fixed package json file, fixed test file

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -2,5 +2,5 @@ describe('first test', () => {
     it('sign in', () => {
         cy.visit('http://automationpractice.com/index.php');
         cy.title().should('contain', 'My Store');
-    })
-})
+    });
+});
